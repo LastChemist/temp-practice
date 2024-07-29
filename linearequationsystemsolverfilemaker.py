9eeb210c378bfa1dcc686e6391e59bbdd0bc9d6c
Last Chemist <lastchemist@gmail.com>
2024-07-29 22:25:10 +0330

    defined class and init

--- linearequationsystemsolverfilemaker.py
+++ linearequationsystemsolverfilemaker.py
@@ -1,3 +1,32 @@
 import util.assets
 import os
 import linearequationsystemgenerator
+
+
+class LinearEquationSystemSolverFileMaker:
+
+    def __init__(self, chemical_equation: str) -> None:
+
+        self.chemical_equation = chemical_equation
+        self.current_directory = os.path.dirname(__file__)
+        equation_generator_object = (
+            linearequationsystemgenerator.LinearEquationsSystemGenerator(
+                chemical_equation=self.chemical_equation
+            )
+        )
+
+        self.system_of_linear_equations = (
+            equation_generator_object.generateEquationSystem()
+        )
+        self.num_of_variables = (
+            equation_generator_object.demand_for_variables_to_solve_count
+        )
+        self.symbols_list = util.assets.parameter_symbols
+        self.variables_str = ""
+
+        for i, symbol in enumerate(self.symbols_list):
+            self.variables_str += symbol
+            if self.num_of_variables == i:
+                break
+            else:
+                self.variables_str += ","
