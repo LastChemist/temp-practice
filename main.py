2bb4abba53fad89b998f951ef0d97d0ad38507ff
Last Chemist <lastchemist@gmail.com>
2024-07-29 22:55:35 +0330

    defined execute

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import linearequationsystemsolverfilemaker
 import chemicalequationrewriter
+
+
+def execute(input_chemical_equation: str) -> None:
+
+    linear_equations_system_object: object = (
+        linearequationsystemsolverfilemaker.LinearEquationSystemSolverFileMaker(
+            chemical_equation=input_chemical_equation
+        )
+    )
+    linear_equations_system_object.generateEquationAndSaveSolverFile()
+    linear_equations_system_object.executeSolverFile()
+    return chemicalequationrewriter.ChemicalReactionRewriter().executeRewriter(
+        chemical_equation=input_chemical_equation
+    )
+
