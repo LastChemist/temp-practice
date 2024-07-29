31ab6d2935d20095135220ebe591fa96376a7127
Last Chemist <lastchemist@gmail.com>
2024-07-29 22:34:52 +0330

    defined class and init

--- chemicalequationrewriter.py
+++ chemicalequationrewriter.py
@@ -2,3 +2,22 @@ import os
 from sympy import sympify
 import equationparser
 import util.jsonhandler
+
+
+class ChemicalReactionRewriter:
+    def __init__(self) -> None:
+
+        self.parent_folder_directory = os.path.dirname(__file__)
+        self.equation_solution: tuple = ()
+        self.chemical_formulas_dict = {}
+        self.reactants_list: list[str] = []
+        self.products_list: list[str] = []
+        # self.last_parameter: str = ""
+
+        self.assigned_reactants_list: list[str] = []
+        self.assigned_products_list: list[str] = []
+
+        self.json_handler_object: object = util.jsonhandler.Handler(
+            file_path=rf"{self.parent_folder_directory}"
+        )
+        self.json_handler_object.read()
