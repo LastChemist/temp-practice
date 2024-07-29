ef86bb6ebf63d44870e67d59315e6c68463f3375
Last Chemist <lastchemist@gmail.com>
2024-07-29 14:56:51 +0330

    Add init and write to the Handler class

---
+++ util/JsonHandler.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+
+class Handler:
+    content: dict = {"equation_solution": "", "error_msg": ""}
+
+    def __init__(self, file_path: str, content=content) -> None:
+        self.content = content
+        self.file_path: str = rf"{file_path}\data.json"
+        if not os.path.isfile(self.file_path):
+            self.write(content=self.content)
+
+    def write(self, content: dict = content):
+        with open(self.file_path, "w") as file:
+            json.dump(content, file)
